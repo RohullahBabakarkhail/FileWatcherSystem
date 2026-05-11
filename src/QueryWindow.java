ca77a2329db9154c410fd465f48283ce5675d27a
RohullahBabakarkhail <rohullahsameer@gmail.com>
2026-05-10 19:30:18 -0700

    Update QueryWindow.java

--- src/QueryWindow.java
+++ src/QueryWindow.java
@@ -12,22 +12,33 @@ public class QueryWindow extends JFrame {
 
     private void setupWindow() {
         setTitle("Query Database");
-        setSize(600, 400);
-        setLayout(new BorderLayout());
+        setSize(650, 400);
+        setLayout(new BorderLayout(10, 10));
 
+        createMenuBar();
+        createMainPanel();
+    }
+
+    private void createMenuBar() {
         JMenuBar menuBar = new JMenuBar();
+
         JMenu databaseMenu = new JMenu("Database");
+
         JMenuItem clearItem = new JMenuItem("Clear Database");
         JMenuItem closeItem = new JMenuItem("Return to Main Window");
 
-        clearItem.addActionListener(e -> resultsArea.append("Database cleared.\n"));
+        clearItem.addActionListener(e -> clearDatabasePlaceholder());
         closeItem.addActionListener(e -> dispose());
 
         databaseMenu.add(clearItem);
         databaseMenu.add(closeItem);
-        menuBar.add(databaseMenu);
-        setJMenuBar(menuBar);
 
+        menuBar.add(databaseMenu);
+
+        setJMenuBar(menuBar);
+    }
+
+    private void createMainPanel() {
         JPanel topPanel = new JPanel(new GridLayout(2, 2, 10, 10));
 
         queryTypeBox = new JComboBox<>(new String[]{
@@ -38,9 +49,11 @@ public class QueryWindow extends JFrame {
         });
 
         searchField = new JTextField();
-        JButton searchButton = new JButton("Search");
 
-        searchButton.addActionListener(e -> search());
+        JButton searchButton = new JButton("Search");
+        searchButton.addActionListener(e -> searchPlaceholder());
+
+        topPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         topPanel.add(new JLabel("Query Type:"));
         topPanel.add(queryTypeBox);
@@ -49,16 +62,24 @@ public class QueryWindow extends JFrame {
 
         resultsArea = new JTextArea();
         resultsArea.setEditable(false);
+        resultsArea.setText("Query Database Window\nSearch features will be completed in a later iteration.\n");
 
         add(topPanel, BorderLayout.NORTH);
         add(new JScrollPane(resultsArea), BorderLayout.CENTER);
         add(searchButton, BorderLayout.SOUTH);
     }
 
-    private void search() {
+    private void searchPlaceholder() {
         String queryType = queryTypeBox.getSelectedItem().toString();
         String value = searchField.getText();
 
-        resultsArea.append("Searching by " + queryType + ": " + value + "\n");
+        resultsArea.append("Search clicked.\n");
+        resultsArea.append("Query Type: " + queryType + "\n");
+        resultsArea.append("Search Value: " + value + "\n");
+        resultsArea.append("Note: Real database search will be added in a later iteration.\n\n");
+    }
+
+    private void clearDatabasePlaceholder() {
+        resultsArea.append("Clear Database clicked. This is currently a placeholder.\n");
     }
 }
