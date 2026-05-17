e930e1e1c533bee91d5a21042537e1926d331436
RohullahBabakarkhail <rohullahsameer@gmail.com>
2026-05-17 14:15:06 -0700

    Update DatabaseManager.java

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -13,22 +13,27 @@ public class DatabaseManager {
     }
 
     public void saveEvent(FileEvent event) {
-        System.out.println("Save event placeholder: " + event);
+        System.out.println("Save event placeholder:");
+        System.out.println(event);
     }
 
     public List<FileEvent> getEventsByExtension(String extension) {
+        System.out.println("Search by extension placeholder: " + extension);
         return new ArrayList<>();
     }
 
     public List<FileEvent> getEventsByFileName(String fileName) {
+        System.out.println("Search by file name placeholder: " + fileName);
         return new ArrayList<>();
     }
 
     public List<FileEvent> getEventsByEventType(String eventType) {
+        System.out.println("Search by event type placeholder: " + eventType);
         return new ArrayList<>();
     }
 
     public List<FileEvent> getEventsByDate(String date) {
+        System.out.println("Search by date placeholder: " + date);
         return new ArrayList<>();
     }
 
