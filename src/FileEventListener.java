737f16a69d75293b49d74251511b3ac8d352fcbd
RohullahBabakarkhail <rohullahsameer@gmail.com>
2026-06-08 22:28:47 -0700

    Update FileEventListener.java

--- src/FileEventListener.java
+++ src/FileEventListener.java
@@ -1,5 +1,22 @@
+/**
+ * Listener interface used by FileMonitor to send detected events
+ * and monitor messages back to the GUI.
+ *
+ * @author Rohullah Babakarkhail
+ * @author Kalsoom Babakarkhail
+ */
 public interface FileEventListener {
+    /**
+     * Called when a file event is detected.
+     *
+     * @param event the detected file event
+     */
     void onFileEvent(FileEvent event);
 
+    /**
+     * Called when the monitor needs to display a message.
+     *
+     * @param message the message to display
+     */
     void onMonitorMessage(String message);
 }
