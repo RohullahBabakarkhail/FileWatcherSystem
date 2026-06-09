bb843a3aa435f8b5d40ecda538694355a088961d
RohullahBabakarkhail <rohullahsameer@gmail.com>
2026-06-08 22:29:26 -0700

    Update FileWatcherApp.java

--- src/FileWatcherApp.java
+++ src/FileWatcherApp.java
@@ -1,18 +1,39 @@
+/**
+ * Main application starter for the File Watcher System.
+ * This class creates and starts the main GUI window.
+ *
+ * @author Rohullah Babakarkhail
+ * @author Kalsoom Babakarkhail
+ */
 public class FileWatcherApp {
     private MainWindow mainWindow;
 
+    /**
+     * Constructs the application and creates the main window.
+     */
     public FileWatcherApp() {
         mainWindow = new MainWindow();
     }
 
+    /**
+     * Starts the application by showing the main window.
+     */
     public void start() {
         mainWindow.showWindow();
     }
 
+    /**
+     * Stops the application.
+     */
     public void stop() {
         System.exit(0);
     }
 
+    /**
+     * Main method used to run the File Watcher System.
+     *
+     * @param args command-line arguments, not used
+     */
     public static void main(String[] args) {
         FileWatcherApp app = new FileWatcherApp();
         app.start();
